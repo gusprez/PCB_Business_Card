2ae22dd36bf6301392048b2d9fb029a030f34cce
Gusprez <alphapapamike91@outlook.com>
2021-03-23 08:38:52 +0100

    Anyadido array para numeros de pines para leds y anyadido setup del pin del boton con interrupcion

--- PCB_Business_Card_FW/PCB_Business_Card_FW.ino
+++ PCB_Business_Card_FW/PCB_Business_Card_FW.ino
@@ -1,12 +1,35 @@
+boolean toggle1 = 1;
+const uint8_t led_pin_array[] = {5, 6, 7, 8, 9, 10, 11, 12}; // Array con pines de led indicadores de intervalos de 5min
+const uint8_t select_button_pin = 2;
+
+void ISR_button_pin() {
+    if ( toggle1 ) {
+        digitalWrite( 12, HIGH );
+        toggle1 = 0;
+    }
+    else {
+        digitalWrite( 12, LOW );
+        toggle1 = 1;
+    }
+}
 
 void setup() {
-    pinMode( 2, OUTPUT );
+    pinMode( select_button_pin, INPUT_PULLUP );
+    attachInterrupt( digitalPinToInterrupt( select_button_pin ), ISR_button_pin, FALLING );
+
+    for ( uint8_t ii = 0; ii < sizeof( led_pin_array ); ++ii ) {
+        pinMode( led_pin_array[ii], OUTPUT );
+    }
 
 }
 
 void loop() {
-    digitalWrite( 2, HIGH );
-    delay(500);
-    digitalWrite( 2, LOW );
-    delay(500);
+    for ( uint8_t ii = 0; ii < sizeof( led_pin_array ); ++ii ) {
+        digitalWrite( led_pin_array[ii], HIGH );
+        delay(500);
+    }
+    for ( uint8_t ii = 0; ii < sizeof( led_pin_array ); ++ii ) {
+        digitalWrite( led_pin_array[ii], LOW );
+        delay(500);
+    }
 }
